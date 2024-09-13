b199fabe27e856d45094db49ad1039aac61bc7e2
nhy <gusdlnam@gmail.com>
2024-09-13 13:56:13 +0900

    add brand item page

---
+++ frontend/src/js/product.js
@@ -0,0 +1,24 @@
+setTitle();
+
+function setTitle() {
+  const params = new URLSearchParams(location.search);
+  let brand = params.get("sort");
+  let text = "";
+
+  switch (brand) {
+    case "maxbone":
+      text = "MAXBONE";
+      break;
+    case "petsochic":
+      text = "PET SO CHIC";
+      break;
+    case "milkandpepper":
+      text = "MILK & PEPPER";
+      break;
+
+    default:
+      break;
+  }
+
+  $(".title").text(text);
+}
