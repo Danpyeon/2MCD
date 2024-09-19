f0c3152e8240353db70a6f4bebad19146822293d
nhy <gusdlnam@gmail.com>
2024-09-20 03:01:10 +0900

    add cart api

--- frontend/src/js/mypage.js
+++ frontend/src/js/mypage.js
@@ -1,101 +1,54 @@
-const history = [
-  {
-    ordernumber: 20222,
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    price: 9500,
-    cnt: 1,
-  },
-  {
-    ordernumber: 20222,
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    price: 9500,
-    cnt: 1,
-  },
-];
-
-const interests = [
-  {
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    price: 9500,
-  },
-  {
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    price: 9500,
-  },
-];
-
-const review = [
-  {
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    review: "sjansjan rnldudnj~",
-  },
-  {
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    review: "sjansjan rnldudnj~",
-  },
-];
-
-const cart = [
-  {
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    price: 9500,
-    cnt: 1,
-  },
-  {
-    name: "밀크앤페퍼 카마라 자켓 토프",
-    price: 9500,
-    cnt: 1,
-  },
-];
-
 loadInfo();
 async function loadInfo() {
-  let itemList;
+  let cart;
+  let id = getCookies("userCache");
 
-  // await $.ajax({
-  //   type: "post",
-  //   url: "http://localhost:3000/mypage", // 나중에 수정!
-  //   contentType: "application/json",
-  //   success: (res) => {
-  //     itemList = res;
-  //   },
-  //   error: (e) => {
-  //     itemList = e;
-  //   },
-  // });
+  let obj = {
+    id: id,
+  };
 
-  $(".history-cnt").text(history.length);
-  $(".inter-cnt").text(interests.length);
-  $(".review-cnt").text(review.length);
+  await $.ajax({
+    type: "post",
+    url: "http://localhost:3000/mypageCart",
+    data: JSON.stringify(obj),
+    contentType: "application/json",
+    success: (res) => {
+      cart = res.data;
+    },
+    error: (e) => {
+      itemList = e;
+    },
+  });
 
-  // $(".history-cnt").text(itemList.history.length);
-  // $(".inter-cnt").text(itemList.interests.length);
-  // $(".review-cnt").text(itemList.review.length);
-
-  // $(".history > item-list").html(makeHistoryList(itemList.history));
-  // $(".interests > item-list").html(makeHistoryList(itemList.interests));
-  // $(".review > item-list").html(makeHistoryList(itemList.review));
-  // $(".cart > item-list").html(makeHistoryList(itemList.cart));
-  $(".history > .item-list").html(makeHistoryList(history));
-  $(".interests > .item-list").html(makeHistoryList(interests));
-  $(".review > .item-list").html(makeHistoryList(review));
   $(".cart > .item-list").html(makeHistoryList(cart));
 }
 
+function getCookies(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(";");
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1);
+    if (c.indexOf(nameEQ) == 0) {
+      return c.substring(nameEQ.length, c.length);
+    }
+  }
+  return null;
+}
+
 function makeHistoryList(list) {
-  if (!list) {
-    return "<span>관심상품이 없습니다.</span>";
+  if (list === undefined || list === null || !list) {
+    return "<div class='item'><span>관심상품이 없습니다.</span></div>";
   }
 
   let text = "";
-  for (let i = 0; i < list.length; i++) {
-    text += "<div class=item>";
-    if ("ordernumber" in list[i]) text += `<span>${list[i].ordernumber}</span>`;
-    text += `<span>${list[i].name}</span>`;
-    if ("price" in list[i]) text += `<span>${list[i].price}원</span>`;
-    if ("cnt" in list[i]) text += `<span>${list[i].cnt}</span>`;
-    if ("review" in list[i])
-      text += `<span>${cuttingText(list[i].review)}</span>`;
+  let max = list.length > 2 ? 2 : list.length;
+  for (let i = 0; i < max; i++) {
+    text += "<div class='item'>";
+    text += `<span>${list[i].item_seq}</span>`;
+    text += `<span>${cuttingText(list[i].item_name)}</span>`;
+    text += `<span>${list[i].item_price}원</span>`;
+    text += `<span>1</span>`;
     text += "</div>";
   }
 
