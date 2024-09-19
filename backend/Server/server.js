cf210b6dca4a06c6b8dc9d77732eabb68c0e5e51
JYP <tntn3332@gmail.com>
2024-09-19 12:06:55 +0900

    unfinished api ( maybe 90persent )

--- backend/Server/server.js
+++ backend/Server/server.js
@@ -20,30 +20,33 @@ app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
 
-// 데이터 가져오는 API (item_tb와 file_tb를 outer join)
 app.get('/api/items', (req, res) => {
     const page = parseInt(req.query.page) || 1; // 쿼리에서 페이지 번호 가져옴, 기본값은 1
+    const cate = req.query.cate || 'all';
     const limit = 18; // 한 페이지당 항목 수
     const offset = (page - 1) * limit;
+    
+    console.log(req.query);
 
     // item_tb와 file_tb 조인하여 필요한 데이터 가져오기
     const query = `
-        with FILE as (select * from FILE_TB
-    where FILE_SEQ in (select MIN(FILE_SEQ) as FILE_SEQ from FILE_TB
-      group by ITEM_SEQ
-      order by ITEM_SEQ
-       ))
-       select
-       i.item_seq
-       , i.detail_category
-       , i.brand, i.item_name
-       , f.item_files 
+        WITH FILE AS (SELECT * FROM FILE_TB
+        WHERE FILE_SEQ IN (SELECT MIN(FILE_SEQ) AS FILE_SEQ FROM FILE_TB
+        GROUP BY ITEM_SEQ
+        ORDER BY ITEM_SEQ))
+        SELECT
+        i.item_seq,
+        i.detail_category,
+        i.brand,
+        i.item_name,
+        f.item_files 
         FROM item_tb i 
-        LEFT OUTER join FILE f ON i.item_seq = f.item_seq
-       	order by item_seq desc
+        LEFT OUTER JOIN FILE f ON i.item_seq = f.item_seq
+        WHERE i.detail_category = ?
+        ORDER BY i.item_seq DESC
         LIMIT ? OFFSET ?`;
 
-    connection.query(query, [limit, offset], (err, rows) => {
+    connection.query(query, [cate, limit, offset], (err, rows) => {
         if (err) {
             console.error('DB error:', err);
             res.status(500).json({ error: 'DB 조회 실패' });
@@ -51,7 +54,8 @@ app.get('/api/items', (req, res) => {
             res.status(200).json({
                 status: 200,
                 data: rows,
-                page: page
+                page: page,
+                cate: cate
             });
         }
     });
